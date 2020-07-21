ebd992fd85b3b4c2ba442f1488881bbe3a397618
瞭望者 <49153689+496943@users.noreply.github.com>
2020-07-21 15:36:37 +0800

    ribbon负载均衡

--- ribbon-user/src/main/java/com/wzx/example/ribbonuser/RibbonUserApplication.java
+++ ribbon-user/src/main/java/com/wzx/example/ribbonuser/RibbonUserApplication.java
@@ -2,7 +2,9 @@ package com.wzx.example.ribbonuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.wzx.example")
 @SpringBootApplication
 public class RibbonUserApplication {
 
--- ribbon-user/src/main/java/com/wzx/example/ribbonuser/UserService.java
+++ ribbon-user/src/main/java/com/wzx/example/ribbonuser/UserService.java
@@ -5,6 +5,7 @@ import com.netflix.loadbalancer.RandomRule;
 import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +19,9 @@ public class UserService {
 
     @Autowired
     LoadBalancerClient loadBalancerClient;
-
+    //使用方式2
     @Bean
+    @LoadBalanced
     public void SetRestTemplate(){
         restTemplate = new RestTemplate();
     }
@@ -32,8 +34,20 @@ public class UserService {
 
     @GetMapping("/getUserOrder")
     public String getUserOrder(){
-        ServiceInstance instance = loadBalancerClient.choose("spring-cloud-ribbon-order");
-        String url = String.format("http://%s:%s"+"/getOrderInfo", instance.getHost(), instance.getPort());
-        return  restTemplate.getForObject(url, String.class);
+        return  restTemplate.getForObject("http://spring-cloud-ribbon-order/getOrderInfo", String.class);
     }
+
+//    使用方式1
+//    @Bean
+//    public IRule ribbonRule(){
+//        //设置为随机负载，默认是轮询
+//        return new RandomRule();
+//    }
+//
+//    @GetMapping("/getUserOrder")
+//    public String getUserOrder(){
+//        ServiceInstance instance = loadBalancerClient.choose("spring-cloud-ribbon-order");
+//        String url = String.format("http://%s:%s"+"/getOrderInfo", instance.getHost(), instance.getPort());
+//        return  restTemplate.getForObject(url, String.class);
+//    }
 }
